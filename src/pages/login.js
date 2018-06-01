3bed7987c34509ad7ec0df05abbceae72b0db5c3
huqiliang <181831335@qq.com>
2018-06-01 17:35:47 +0800

    login.js

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,14 +1,60 @@
 import React, { Component } from "react";
-import { View } from "react-native";
-import { InputItem, Button } from "antd-mobile";
+import { View, Text, StyleSheet, Div, TextInput } from "react-native";
+import { List, InputItem, Button, Flex } from "antd-mobile";
+// import { createForm } from "rc-form";
 
 export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      hotelCode: ""
+    };
+  }
+  hotelCodeChange() {
+    this.setState({
+      hotelCode: "ddd"
+    });
+  }
+  pullPage() {
+    // todo 拉取远程数据
+  }
   render() {
     return (
-      <View>
-        集团代码:<InputItem />
-        <Button type="primary">确定</Button>
+      <View style={styles.container}>
+        <List style={styles.list}>
+          <InputItem
+            labelNumber="5"
+            style={styles.list}
+            clear
+            placeholder="请输入酒店代码"
+            onChange={e => {
+              this.hotelCodeChange;
+            }}
+          >
+            酒店代码:
+          </InputItem>
+        </List>
+        <View style={styles.button}>
+          <Button type="primary" onClick={this.pullPage}>
+            确定
+          </Button>
+        </View>
       </View>
     );
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
+  list: {
+    width: "50%"
+  },
+  button: {
+    marginTop: "5%"
+  }
+});
